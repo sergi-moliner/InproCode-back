d7d56dcc12303731b8c2b99d0491c1845919df54
sergi.moliner <sergilopezmoliner@gmail.com>
2024-06-10 17:24:22 +0200

    final commit

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('inprocode', 'root', 'R%7$#qt5', {
+const sequelize = new Sequelize('inprocode', 'root', '*******', {
     host: 'localhost',
     dialect: 'mysql'
 });
