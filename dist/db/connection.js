8073a779c41d52012ed9ea8dcf4ff74171efc0b2
sergi.moliner <sergilopezmoliner@gmail.com>
2024-06-10 17:28:12 +0200

    final fix

--- dist/db/connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('inprocode', 'root', '*******', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-exports.default = sequelize;
