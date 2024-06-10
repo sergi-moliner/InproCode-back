ef6a79dc6413e97907b0c06c618da24e515bc836
sergi.moliner <sergilopezmoliner@gmail.com>
2024-06-10 17:31:22 +0200

    env

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('inprocode', 'root', '*******', {
+const sequelize = new Sequelize('inprocode', 'root', 'R%7$#qt5', {
     host: 'localhost',
     dialect: 'mysql'
 });
