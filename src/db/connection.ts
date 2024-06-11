5521c73593b48db16448613b7e5a1806b266b45c
sergi.moliner <sergilopezmoliner@gmail.com>
2024-06-11 12:15:51 +0200

    database

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('inprocode', 'root', '*******', {
+const sequelize = new Sequelize('inprocode', 'root', 'xxxxx', {
     host: 'localhost',
     dialect: 'mysql'
 });
